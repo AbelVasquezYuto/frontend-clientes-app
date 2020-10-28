8928beebb3601826f8db880aed6c8a4b81847c37
AbelVasquezYuto <abelfebrero25@gmail.com>
2020-10-28 11:17:58 -0500

    integrando bootstrap

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',  
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Bienvenidos a Angular';
+
+  curso: string = 'Curso Spring 5 y Angular';
+  profesor: string = 'Abel Vasquez'
+}
